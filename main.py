6b2e49b8c0a6d7c0a89034bf25efa893730e7773
oberon-shared <development.business.shared@gmail.com>
2022-09-20 07:19:34 -0700

    Commit From Python

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def play(data, h):
     print("Hole", h)
     score = get_score()
     if score is None:
-        print("Exiting")
+        print(colored("Exiting", "red"))
         data.abort()
         return 11
     data.add_score(h, score)
